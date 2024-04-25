0072bc25106c9c29d2bf5b33b51a9b85b60a2b14
Gleb Petrov <kotejackwhite@gmail.com>
2024-04-25 18:36:14 +0700

    add db

--- db.sql
+++ db.sql
@@ -1,8 +1,5 @@
 -- Adminer 4.8.1 PostgreSQL 16.2 (Debian 16.2-1.pgdg120+2) dump
--- database: PayBot
 
-
---Статичные данные
 DROP TABLE IF EXISTS "data";
 DROP SEQUENCE IF EXISTS data_id_seq;
 CREATE SEQUENCE data_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1;
@@ -14,8 +11,19 @@ CREATE TABLE "public"."data" (
     CONSTRAINT "data_pkey" PRIMARY KEY ("id")
 ) WITH (oids = false);
 
+INSERT INTO "data" ("id", "type", "text") VALUES
+(2,	'start_two',	'Мы проводим оплаты ежедневно с 8:00 МСК по 20:00 МСК. Если вы начали работу с ботом до конца рабочего дня, но не успели воспользоваться его услугами, то бот продолжает для вас работу. Если бот не получает сообщений от вас в течении 10 минут, то он закрывает заказ. '),
+(1,	'start',	'Здравствуйте!  Данный бот создан @Mar1osh.
 
---Заказы
+Если у вас возникли какие-либо вопросы, ошибки в работе бота или вам нужна дополнительная услуга, то пишите мне в личные сообщения с тегом 
+
+#Оплата_через_бота.'),
+(4,	'to_pay',	'К оплате: {summ}₽ с учетом комисси банка и бота.
+
+Если совершить оплату сервиса/услуги не удастся, мы совершим возврат средств в полном обьеме.
+
+Продолжить?');
+
 DROP TABLE IF EXISTS "order";
 DROP SEQUENCE IF EXISTS order_id_seq;
 CREATE SEQUENCE order_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1;
@@ -33,7 +41,6 @@ CREATE TABLE "public"."order" (
 ) WITH (oids = false);
 
 
---Пользователи
 DROP TABLE IF EXISTS "users";
 DROP SEQUENCE IF EXISTS users_id_seq;
 CREATE SEQUENCE users_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1;
@@ -42,9 +49,12 @@ CREATE TABLE "public"."users" (
     "id" integer DEFAULT nextval('users_id_seq') NOT NULL,
     "username" text NOT NULL,
     "chat_id" text NOT NULL,
-    "state" text NOT NULL,
+    "state" text,
+    "full_name" text,
     CONSTRAINT "users_pkey" PRIMARY KEY ("id")
 ) WITH (oids = false);
 
+INSERT INTO "users" ("id", "username", "chat_id", "state", "full_name") VALUES
+(3,	'Summer_Death',	'1194700554',	NULL,	'Gleb Petrov');
 
--- 2024-04-25 07:08:57.375192+00
+-- 2024-04-25 11:35:48.331592+00
